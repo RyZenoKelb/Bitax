0f8e3075a21e4afaeb8c5b7cd7e75713b24d033b
RyZeno <ryzenoking@gmail.com>
2025-05-01 22:26:51 +0200

    auto update

--- src/app/api/auth/[...nextauth/]/route.ts
+++ src/app/api/auth/[...nextauth/]/route.ts
@@ -1,9 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const handler = NextAuth(authOptions);
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { handler as GET, handler as POST };
