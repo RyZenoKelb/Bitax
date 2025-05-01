de0989dd45906b9f1caed85d7b117003613970f8
RyZeno <ryzenoking@gmail.com>
2025-05-01 14:42:43 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -229,14 +229,15 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* Contenu principal avec animation d'entrée */}
         <main className="flex-grow py-10 px-4 sm:px-6 transition-all duration-300 relative">
           <div className="container mx-auto relative z-10">
-            <Transition
-              show={isLoaded}
-              enter="transition-all duration-700 ease-out"
-              enterFrom="opacity-0 translate-y-10"
-              enterTo="opacity-100 translate-y-0"
-            >
-              <Component {...pageProps} />
-            </Transition>
+            {isLoaded ? (
+              <div className="transition-all duration-700 ease-out transform translate-y-0 opacity-100">
+                <Component {...pageProps} />
+              </div>
+            ) : (
+              <div className="opacity-0 translate-y-10">
+                <Component {...pageProps} />
+              </div>
+            )}
           </div>
         </main>
         
