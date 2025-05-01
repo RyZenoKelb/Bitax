43e988704bede28af08db1a612e2e030f1d39e66
RyZeno <ryzenoking@gmail.com>
2025-05-01 14:14:56 +0200

    auto update

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -25,6 +25,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/guide": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/guide.js"
+    ],
     "/support": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
