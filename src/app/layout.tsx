b1e238565c85b3946ca33748a5e8e3a7257ad492
RyZeno <ryzenoking@gmail.com>
2025-05-03 19:57:54 +0200

    auto update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
-        {/* Ajout des polices Google */}
+        {/* Importation des polices de manière optimisée */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <AuthProvider>
