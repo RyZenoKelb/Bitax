a160c03bc8c904cb140b5f16a160a49a9d3797b4
RyZeno <ryzenoking@gmail.com>
2025-05-05 21:37:47 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -608,3 +608,14 @@ export default function App({ Component, pageProps }: AppProps) {
                         onClick={() => setIsMenuOpen(false)}
                       ></Link>
                         <svg className="w-5 h-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                        </svg>
+                        Déconnexion
+                      </Link>
+                    </div>
+                  </div>
+                </Transition>
+              </header>
+
+              {/* Contenu principal avec animation d'entrée */}
+              <main className="flex-grow py-6 px-4 sm:px-6 transition-all duration-300 relative">
