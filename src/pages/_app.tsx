4ef4119c7aa7d017afbdfa49e44f19ca438d2793
RyZeno <ryzenoking@gmail.com>
2025-05-05 21:28:55 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -302,12 +302,12 @@ export default function App({ Component, pageProps }: AppProps) {
                             >
                               <div className="absolute inset-0">
                                 <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        </svg>
-                      </div>
-                    </Transition>
-                  </div>
-                </button>
-                
+                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                                </svg>
+                              </div>
+                            </Transition>
+                            <Transition
+                              show={theme === 'light'}
                 {/* Menu hamburger mobile amélioré */}
                 <button 
                   onClick={() => setIsMenuOpen(!isMenuOpen)}
