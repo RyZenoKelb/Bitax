e01d42b846a8b50175793e0a699c2dc9a15cb418
RyZeno <ryzenoking@gmail.com>
2025-05-05 21:34:45 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -531,3 +531,8 @@ export default function App({ Component, pageProps }: AppProps) {
                       ></button>
                         <span className="sr-only">Ouvrir le menu</span>
                         <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
