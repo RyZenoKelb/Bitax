47be4e8231606962a2735294f274b5be14795888
RyZeno <ryzenoking@gmail.com>
2025-05-05 14:40:23 +0200

    auto update

---
+++ src/components/layouts/DashboardLayout.tsx
@@ -0,0 +1,6 @@
+
+// src/components/layouts/DashboardLayout.tsx
+import React, { ReactNode, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
