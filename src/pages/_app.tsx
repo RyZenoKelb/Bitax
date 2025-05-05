01c38003c8ca7c8e43a1426de0e97b1bd02bbfe3
RyZeno <ryzenoking@gmail.com>
2025-05-05 21:34:19 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -512,10 +512,16 @@ export default function App({ Component, pageProps }: AppProps) {
                           >
                             <div className="absolute inset-0">
                               <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            </div>
-          </div>
-        </footer>
-      </div>
-    </AuthProvider>
-  );
-}
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                              </svg>
+                            </div>
+                          </Transition>
+                        </div>
+                      </button>
+                      
+                      {/* Menu hamburger mobile amélioré */}
+                      <button 
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        className={`md:hidden p-2.5 rounded-lg ${
+                          theme === 'dark'
+                            ? 'bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-white' 
