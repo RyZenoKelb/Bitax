a1d38c936911aec88b4f9b3082a346491b8e6df4
RyZeno <ryzenoking@gmail.com>
2025-05-05 21:36:04 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -587,3 +587,9 @@ export default function App({ Component, pageProps }: AppProps) {
                         href="/profile" 
                         className={`flex items-center px-3 py-2.5 rounded-lg ${
                           theme === 'dark'
+                            ? 'text-gray-300 hover:bg-gray-800 hover:text-white'
+                            : 'text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                        }`}
+                        onClick={() => setIsMenuOpen(false)}
+                      ></Link>
+                        <svg className="w-5 h-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
