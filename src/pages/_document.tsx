c64a1e1c81b4c0da8c8a3298aa03c00aac629318
RyZeno <ryzenoking@gmail.com>
2025-05-07 22:39:54 +0200

    auto update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,31 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fr">
-      <Head>
-        {/* Polices et méta-données sont déjà chargées dans _app.tsx */}
-      </Head>
+    <Html lang="en">
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
-        
-        {/* Injection du script pour initialiser le background étoilé */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              document.addEventListener('DOMContentLoaded', function() {
-                // Importer et exécuter le script de fond étoilé après chargement de la page
-                import('/scripts/starBackground.js')
-                  .then(module => {
-                    if (typeof module.setupStarBackground === 'function') {
-                      module.setupStarBackground();
-                    }
-                  })
-                  .catch(err => console.warn('Could not load star background:', err));
-              });
-            `,
-          }}
-        />
       </body>
     </Html>
   );
