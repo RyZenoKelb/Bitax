565357216b9aedcf0bdd048f9cda81bf9f94fd67
RyZeno <ryzenoking@gmail.com>
2025-05-07 20:20:32 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -229,7 +229,7 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.pathname]);
 
   return (
-    <SessionProvider></SessionProvider>
+    <SessionProvider>
     <AuthProvider>
       <Head>
         <title>Bitax | Fiscalité crypto redéfinie</title>
