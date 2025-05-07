a6b4ef29f107fe35b7b3a181a2c2b00348f32d57
RyZeno <ryzenoking@gmail.com>
2025-05-07 21:21:29 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -199,9 +199,6 @@ const AppContent = ({ Component, pageProps }: { Component: AppProps['Component']
     handleResize();
     
     // Attendre un peu pour faire l'animation d'apparition
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, 100);
     
     return () => window.removeEventListener('resize', handleResize);
   }, []);
