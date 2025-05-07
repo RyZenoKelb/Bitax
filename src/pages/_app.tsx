0165ec5b8d559b890b18301875572a37293e7b34
RyZeno <ryzenoking@gmail.com>
2025-05-07 19:08:23 +0200

    auto update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -296,7 +296,7 @@ export default function App({ Component, pageProps }: AppProps) {
               if (star.y > height) {
                 star.y = 0;
                 star.x = Math.random() * width;
-              })
+              }
               
               ctx.beginPath();
               if (!ctx) return;
