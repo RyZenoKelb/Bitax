32cd71dc6a42a17e299285b2468fd2945718e4be
RyZeno <ryzenoking@gmail.com>
2025-05-11 19:07:55 +0200

    auto update

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -132,7 +132,7 @@ export async function POST(req: Request) {
     // Si l'utilisateur s'est inscrit avec un code d'invitation, mettre à jour l'entrée de la waiting list
     if (inviteCode) {
       logDebug('Mise à jour de l\'entrée de waiting list avec l\'ID utilisateur');
-      await prisma.WaitingList.update({
+      await prisma.waitingList.update({
         where: { inviteCode },
         data: { userId: user.id }
       });
