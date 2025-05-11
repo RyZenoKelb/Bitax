4bcb2d70fc860e997d04eb53168a14907c20297c
RyZeno <ryzenoking@gmail.com>
2025-05-11 18:35:26 +0200

    auto update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -721,8 +721,8 @@ export default function Home() {
               </Link>
             </div>
 
-                          {/* Badge Early Access */}
-                          <div className="ml-3 hidden sm:block">
+            {/* Badge Early Access */}
+            <div className="ml-3 hidden sm:block">
                 <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gradient-to-r from-purple-500 to-indigo-500 text-white">
                   Early Access
                 </span>
