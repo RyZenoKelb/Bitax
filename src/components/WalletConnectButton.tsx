1721bcdbe5d7bd88249293c913000711923d46dd
RyZeno <ryzenoking@gmail.com>
2025-05-12 17:55:31 +0200

    auto update

--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -471,7 +471,7 @@ const WalletConnectButton: React.FC<WalletConnectButtonProps> = ({
                 const walletId = wallet.id as string;
                 const walletName = wallet.name as string || '';
                 const walletAddress = wallet.address as string;
-                const isCurrentWallet = currentWallet && currentWallet.id === walletId;
+                const isCurrentWallet = currentWallet && currentWallet === walletId;
                 
                 return (
                   <button
