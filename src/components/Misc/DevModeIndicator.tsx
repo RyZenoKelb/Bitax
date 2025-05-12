8c79b3a9ade3a32facc896f56143de60edc82e96
RyZeno <ryzenoking@gmail.com>
2025-05-12 18:28:35 +0200

    auto update

--- src/components/Misc/DevModeIndicator.tsx
+++ src/components/Misc/DevModeIndicator.tsx
@@ -11,7 +11,7 @@ const DevModeIndicator: React.FC = () => {
     if (isDevMode) {
       const timer = setTimeout(() => {
         setIsVisible(false);
-      }, 10000);
+      }, 5000);
       
       return () => clearTimeout(timer);
     }
