30e418fcbce21185223c981cb868554413ee356f
RyZeno <ryzenoking@gmail.com>
2025-05-12 16:13:32 +0200

    auto update

--- script-create-user.js
+++ script-create-user.js
@@ -6,14 +6,14 @@ const prisma = new PrismaClient();
 
 async function createUser() {
   try {
-    const hashedPassword = await hash('250802', 10);
+    const hashedPassword = await hash('250802LGgn', 10);
     
     const user = await prisma.user.create({
       data: {
         name: 'RyZeno',
         email: 'ryzenoking@gmail.com',
         password: hashedPassword,
-        isPremium: false
+        isPremium: true
       }
     });
     
